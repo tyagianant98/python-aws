06a185eb1f11c8e4159c72c8b716ad842189e75f
Anant Tyagi <tyagianant98@gmail.com>
2021-01-18 13:36:07 +0530

    add and multiply

--- pythonanant.py
+++ pythonanant.py
@@ -3,8 +3,18 @@ def multiplyTwoNumbers(x, y):
     return c
 
 
+def addTwoNumbers(x, y):
+    c = int(x) + int(y)
+    return c
+
+
 if __name__ == "__main__":
     a = input("Enter first number: ")
     b = input("Enter second number: ")
     c = multiplyTwoNumbers(a, b)
+    print(c)
+
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+    c = addTwoNumbers(a, b)
     print(c)
