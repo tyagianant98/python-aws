b2e2b7859fcfeec460240af788bf2a9f16fc7c3e
Anant Tyagi <tyagianant98@gmail.com>
2021-01-18 13:25:20 +0530

    multiply

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# python-aws
+def multiplyTwoNumbers(x, y):
+c = int(a) \* int(b)
+return c
+
+if **name** == "**main**":
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+c = multiplyTwoNumbers(a, b)
+print(c)
---
+++ pythonanant.py
@@ -0,0 +1,10 @@
+def multiplyTwoNumbers(x, y):
+    c = int(x) * int(y)
+    return c
+
+
+if __name__ == "__main__":
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+    c = multiplyTwoNumbers(a, b)
+    print(c)
